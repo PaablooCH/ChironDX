8bbb7174b04a6e753d6bf6e0f59c85e2512d994a
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-05-03 16:07:16 +0200

    add[DefaultShader]: merge vertex and pixel

---
+++ Game/Shaders/default.hlsl
@@ -0,0 +1,36 @@
+cbuffer ModelViewProjection : register(b0)
+{
+    matrix MVP;
+};
+
+struct VertexPosColor
+{
+    float3 Position : POSITION;
+    float3 Color    : COLOR;
+};
+
+struct VertexShaderOutput
+{
+    float4 Color    : COLOR;
+    float4 Position : SV_Position;
+};
+
+VertexShaderOutput VSmain(VertexPosColor IN)
+{
+    VertexShaderOutput OUT;
+
+    OUT.Position = mul(MVP, float4(IN.Position, 1.0f));
+    OUT.Color = float4(IN.Color, 1.0f);
+
+    return OUT;
+}
+
+struct PixelShaderInput
+{
+    float4 Color : COLOR;
+};
+
+float4 PSmain(PixelShaderInput IN) : SV_Target
+{
+    return IN.Color;
+}
