b92e080a9c67ea0581a285dd2d0abad08fa061d9
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-05-03 16:21:01 +0200

    add[ModuleCamera]: create view & proj matrices

--- Source/ChironEngine/Source/Modules/ModuleCamera.cpp
+++ Source/ChironEngine/Source/Modules/ModuleCamera.cpp
@@ -1,7 +1,11 @@
 #include "Pch.h"
 #include "ModuleCamera.h"
 
-ModuleCamera::ModuleCamera()
+#include "Application.h"
+
+#include "ModuleWindow.h"
+
+ModuleCamera::ModuleCamera() : _nearPlane(0.1f), _farPlane(10000.f)
 {
 }
 
@@ -11,7 +15,18 @@ ModuleCamera::~ModuleCamera()
 
 bool ModuleCamera::Init()
 {
-	_position = Vector3::Zero;
+	unsigned width;
+	unsigned height;
+
+	App->GetModule<ModuleWindow>()->GetWindowSize(width, height);
+	float aspectRatio = static_cast<float>(width / height);
+
+	_position = Vector3(0, 0, 10);
+
+	_rotation = Quaternion::Identity;
+
+	_view = Matrix::CreateLookAt(_position, Vector3::Zero, Vector3::UnitY);
+	_proj = Matrix::CreatePerspectiveFieldOfView(DirectX::XM_PIDIV4, aspectRatio, _nearPlane, _farPlane);
 	return true;
 }
 
@@ -22,6 +37,20 @@ UpdateStatus ModuleCamera::PreUpdate()
 
 UpdateStatus ModuleCamera::Update()
 {
+	DirectX::Mouse& mouse = DirectX::Mouse::Get();
+	DirectX::Keyboard& keyboard = DirectX::Keyboard::Get();
+
+	const DirectX::Mouse::State& mouseState = mouse.GetState();
+	const DirectX::Keyboard::State& keyState = keyboard.GetState();
+
+	CHIRON_TODO("Finish.");
+	/*if (mouseState.rightButton)
+	{
+
+	}*/
+
+	
+
 	return UpdateStatus::UPDATE_CONTINUE;
 }
 
--- Source/ChironEngine/Source/Modules/ModuleCamera.h
+++ Source/ChironEngine/Source/Modules/ModuleCamera.h
@@ -13,11 +13,40 @@ public:
     UpdateStatus PostUpdate() override;
     bool CleanUp() override;
 
+    // ------------- GETTERS ----------------------
+
+    inline const Vector3& GetPosition();
+    inline const Quaternion& GetRotation();
+    inline const Matrix& GetViewMatrix();
+    inline const Matrix& GetProjMatrix();
+
 private:
     Vector3 _position;
     Quaternion _rotation;
 
     Matrix _view;
     Matrix _proj;
+
+    float _nearPlane;
+    float _farPlane;
 };
 
+inline const Vector3& ModuleCamera::GetPosition()
+{
+    return _position;
+}
+
+inline const Quaternion& ModuleCamera::GetRotation()
+{
+    return _rotation;
+}
+
+inline const Matrix& ModuleCamera::GetViewMatrix()
+{
+    return _view;
+}
+
+inline const Matrix& ModuleCamera::GetProjMatrix()
+{
+    return _proj;
+}
