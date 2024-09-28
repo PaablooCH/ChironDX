1bce477b775585514128fe5e163b6948a0539b5f
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-09-28 22:31:13 +0200

    fix: link and char

--- Source/ChironEngine/Source/DataModels/Window/AboutWindow.cpp
+++ Source/ChironEngine/Source/DataModels/Window/AboutWindow.cpp
@@ -35,14 +35,11 @@ void AboutWindow::Draw(const std::shared_ptr<CommandList>& commandList)
             ImGui::Text("CHIRON ENGINE v.0.1");
             ImGui::Text("3D Game engine made with C++ & DirectX12, created and developed by");
             ImGui::SameLine();
-            ImGui::TextColored(ImVec4(0.0f, 0.5f, 1.0f, 1.0f), "Pablo CH");
-            if (ImGui::IsItemClicked()) {
-                Chiron::Utils::OpenLink(REPOSITORY_URL);
-            }
+            ImGui::TextLinkOpenURL("Pablo CH", REPOSITORY_URL);
             ImGui::SameLine(0.f, 0.f);
             ImGui::Text(".");
             ImGui::Separator();
-            ImGui::Text("Copyright 2024 Pablo Cebollada Hernández");
+            ImGui::Text("Copyright 2024 Pablo Cebollada Hernandez");
             ImGui::TextWrapped("Licensed under the Apache License, Version 2.0 (the \"License\"); you may not use this file except in compliance with the License.");
             ImGui::TextWrapped("Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an \"AS IS\" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.");
         }
