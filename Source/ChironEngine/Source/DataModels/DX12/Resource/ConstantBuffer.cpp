18e0ef03c24331153a0e6590ae47a9626d7b30e1
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-10-02 13:15:56 +0200

    style: translate spanish comment

--- Source/ChironEngine/Source/DataModels/DX12/Resource/ConstantBuffer.cpp
+++ Source/ChironEngine/Source/DataModels/DX12/Resource/ConstantBuffer.cpp
@@ -28,6 +28,6 @@ void ConstantBuffer::CreateView()
     cbvDesc.BufferLocation = _resource->GetGPUVirtualAddress();
     cbvDesc.SizeInBytes = static_cast<UINT>(Chiron::Utils::AlignUp(_sizeInBytes, D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT));
 
-    // Crear la CBV
+    // Create CBV
     _device->CreateConstantBufferView(&cbvDesc, _constantBufferView.GetCPUDescriptorHandle());
 }
